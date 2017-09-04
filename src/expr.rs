2edad7753abb16ac15ef4e400f03fe90fd831734
HMPerson1 <hmperson1@gmail.com>
2017-09-04 11:05:56 -0400

    Optimize expression evaluation

--- src/expr.rs
+++ src/expr.rs
@@ -4,20 +4,34 @@ use std::hash::BuildHasher;
 use std::fmt::{Debug, Display, Formatter};
 use std::collections::HashMap;
 
+use fnv::FnvHashMap;
+
 pub struct Equation(pub Expr, pub Expr);
 
-pub enum Expr {
-    Add(Box<Expr>, Box<Expr>),
-    Sub(Box<Expr>, Box<Expr>),
-    Mul(Box<Expr>, Box<Expr>),
-    Div(Box<Expr>, Box<Expr>),
-    Pow(Box<Expr>, Box<Expr>),
-    Func(KnownFunc, Box<Expr>),
+#[derive(Debug)]
+pub struct Expr(BaseExpr<String>);
+
+enum BaseExpr<V> {
+    Add(Box<BaseExpr<V>>, Box<BaseExpr<V>>),
+    Sub(Box<BaseExpr<V>>, Box<BaseExpr<V>>),
+    Mul(Box<BaseExpr<V>>, Box<BaseExpr<V>>),
+    Div(Box<BaseExpr<V>>, Box<BaseExpr<V>>),
+    Pow(Box<BaseExpr<V>>, Box<BaseExpr<V>>),
+    Func(KnownFunc, Box<BaseExpr<V>>),
     Lit(f64),
-    Var(String),
+    Var(V),
 }
 
+/// invariant: all BaseExpr::Var(usize) in `expr` and values in `varmap`
+/// are valid indices into `varstore`
 #[derive(Debug)]
+pub struct CompiledExpr {
+    expr: BaseExpr<usize>,
+    varmap: FnvHashMap<String, usize>,
+    varstore: Vec<f64>,
+}
+
+#[derive(Debug, Copy, Clone)]
 pub enum KnownFunc {
     AbsoluteValue,
     Floor,
@@ -42,49 +56,49 @@ pub enum KnownFunc {
 pub mod helper {
     use expr::*;
     pub fn add(e1: Expr, e2: Expr) -> Expr {
-        match (e1, e2) {
-            (Expr::Lit(l1), Expr::Lit(l2)) => Expr::Lit(l1 + l2),
-            (e1, e2) => Expr::Add(Box::new(e1), Box::new(e2)),
+        match (e1.0, e2.0) {
+            (BaseExpr::Lit(l1), BaseExpr::Lit(l2)) => Expr(BaseExpr::Lit(l1 + l2)),
+            (e1, e2) => Expr(BaseExpr::Add(Box::new(e1), Box::new(e2))),
         }
     }
     pub fn sub(e1: Expr, e2: Expr) -> Expr {
-        match (e1, e2) {
-            (Expr::Lit(l1), Expr::Lit(l2)) => Expr::Lit(l1 - l2),
-            (e1, e2) => Expr::Sub(Box::new(e1), Box::new(e2)),
+        match (e1.0, e2.0) {
+            (BaseExpr::Lit(l1), BaseExpr::Lit(l2)) => Expr(BaseExpr::Lit(l1 - l2)),
+            (e1, e2) => Expr(BaseExpr::Sub(Box::new(e1), Box::new(e2))),
         }
     }
     pub fn mul(e1: Expr, e2: Expr) -> Expr {
-        match (e1, e2) {
-            (Expr::Lit(l1), Expr::Lit(l2)) => Expr::Lit(l1 * l2),
-            (e1, e2) => Expr::Mul(Box::new(e1), Box::new(e2)),
+        match (e1.0, e2.0) {
+            (BaseExpr::Lit(l1), BaseExpr::Lit(l2)) => Expr(BaseExpr::Lit(l1 * l2)),
+            (e1, e2) => Expr(BaseExpr::Mul(Box::new(e1), Box::new(e2))),
         }
     }
     pub fn div(e1: Expr, e2: Expr) -> Expr {
-        match (e1, e2) {
-            (Expr::Lit(l1), Expr::Lit(l2)) => Expr::Lit(l1 / l2),
-            (e1, e2) => Expr::Div(Box::new(e1), Box::new(e2)),
+        match (e1.0, e2.0) {
+            (BaseExpr::Lit(l1), BaseExpr::Lit(l2)) => Expr(BaseExpr::Lit(l1 / l2)),
+            (e1, e2) => Expr(BaseExpr::Div(Box::new(e1), Box::new(e2))),
         }
     }
     pub fn pow(e1: Expr, e2: Expr) -> Expr {
-        match (e1, e2) {
-            (Expr::Lit(l1), Expr::Lit(l2)) => Expr::Lit(l1.powf(l2)),
-            (e1, e2) => Expr::Pow(Box::new(e1), Box::new(e2)),
+        match (e1.0, e2.0) {
+            (BaseExpr::Lit(l1), BaseExpr::Lit(l2)) => Expr(BaseExpr::Lit(l1.powf(l2))),
+            (e1, e2) => Expr(BaseExpr::Pow(Box::new(e1), Box::new(e2))),
         }
     }
     pub fn neg(e: Expr) -> Expr {
         mul(e, lit(-1.0))
     }
     pub fn func(f: KnownFunc, e: Expr) -> Expr {
-        match e {
-            Expr::Lit(v) => Expr::Lit(f.eval(v)),
-            e => Expr::Func(f, Box::new(e)),
+        match e.0 {
+            BaseExpr::Lit(v) => Expr(BaseExpr::Lit(f.eval(v))),
+            e => Expr(BaseExpr::Func(f, Box::new(e))),
         }
     }
     pub fn lit(v: f64) -> Expr {
-        Expr::Lit(v)
+        Expr(BaseExpr::Lit(v))
     }
     pub fn var<S: Into<String>>(v: S) -> Expr {
-        Expr::Var(v.into())
+        Expr(BaseExpr::Var(v.into()))
     }
 }
 
@@ -94,19 +108,130 @@ pub enum EvalError<'a> {
 }
 
 impl Equation {
-    pub fn vars(&self) -> HashMap<&str, f64> {
-        let mut r = self.0.vars();
-        r.extend(self.1.vars());
-        r
-    }
+    // pub fn vars(&self) -> HashMap<&str, f64> {
+    //     let mut r = self.0.vars();
+    //     r.extend(self.1.vars());
+    //     r
+    // }
     pub fn eval_diff<S: BuildHasher>(&self, env: &HashMap<&str, f64, S>) -> Result<f64, EvalError> {
-        self.0.eval(env).and_then(|l| self.1.eval(env).map(|r| l-r))
+        self.0.eval(env).and_then(|l| self.1.eval(env).map(|r| l - r))
+    }
+
+    pub fn to_diff(self) -> Expr {
+        helper::sub(self.0, self.1)
     }
 }
 
 impl Expr {
     pub fn eval<S: BuildHasher>(&self, env: &HashMap<&str, f64, S>) -> Result<f64, EvalError> {
-        use self::Expr::*;
+        self.0.eval(env)
+    }
+    // pub fn vars(&self) -> HashMap<&str, f64> {
+    //     use self::BaseExpr::*;
+    //     match self.0 {
+    //         Add(ref a, ref b) |
+    //         Sub(ref a, ref b) |
+    //         Mul(ref a, ref b) |
+    //         Div(ref a, ref b) |
+    //         Pow(ref a, ref b) => {
+    //             let mut r = Expr(**a).vars();
+    //             r.extend(Expr(**b).vars());
+    //             r
+    //         }
+    //         Func(_, ref a) => Expr(**a).vars(),
+    //         Lit(_) => HashMap::new(),
+    //         Var(ref a) => {
+    //             let mut r = HashMap::new();
+    //             r.insert(a.as_str(), 0.0);
+    //             r
+    //         }
+    //     }
+    // }
+
+    pub fn compile(self) -> CompiledExpr {
+        let mut map = FnvHashMap::default();
+        let mut store = Vec::new();
+        let e = self.vecvar0(&mut map, &mut store);
+        map.shrink_to_fit();
+        store.shrink_to_fit();
+        CompiledExpr {
+            expr: e,
+            varmap: map,
+            varstore: store,
+        }
+    }
+
+    fn vecvar0(self,
+               varmap: &mut FnvHashMap<String, usize>,
+               varstore: &mut Vec<f64>)
+               -> BaseExpr<usize> {
+        use self::BaseExpr::*;
+        match self.0 {
+            Add(a, b) => {
+                let a = Expr(*a).vecvar0(varmap, varstore);
+                let b = Expr(*b).vecvar0(varmap, varstore);
+                Add(Box::new(a), Box::new(b))
+            }
+            Sub(a, b) => {
+                let a = Expr(*a).vecvar0(varmap, varstore);
+                let b = Expr(*b).vecvar0(varmap, varstore);
+                Sub(Box::new(a), Box::new(b))
+            }
+            Mul(a, b) => {
+                let a = Expr(*a).vecvar0(varmap, varstore);
+                let b = Expr(*b).vecvar0(varmap, varstore);
+                Mul(Box::new(a), Box::new(b))
+            }
+            Div(a, b) => {
+                let a = Expr(*a).vecvar0(varmap, varstore);
+                let b = Expr(*b).vecvar0(varmap, varstore);
+                Div(Box::new(a), Box::new(b))
+            }
+            Pow(a, b) => {
+                let a = Expr(*a).vecvar0(varmap, varstore);
+                let b = Expr(*b).vecvar0(varmap, varstore);
+                Pow(Box::new(a), Box::new(b))
+            }
+            Func(f, a) => {
+                let a = Expr(*a).vecvar0(varmap, varstore);
+                Func(f, Box::new(a))
+            }
+            Lit(l) => Lit(l),
+            Var(a) => {
+                use std::collections::hash_map::Entry;
+                Var(match varmap.entry(a) {
+                    Entry::Occupied(entry) => *entry.get(),
+                    Entry::Vacant(entry) => {
+                        let n = varstore.len();
+                        varstore.push(0.0);
+                        entry.insert(n);
+                        n
+                    },
+                })
+            }
+        }
+    }
+}
+
+impl BaseExpr<usize> {
+    fn eval(&self, env: &[f64]) -> f64 {
+        use self::BaseExpr::*;
+        match *self {
+            Add(ref a, ref b) => a.eval(env) + b.eval(env),
+            Sub(ref a, ref b) => a.eval(env) - b.eval(env),
+            Mul(ref a, ref b) => a.eval(env) * b.eval(env),
+            Div(ref a, ref b) => a.eval(env) / b.eval(env),
+            Pow(ref a, ref b) => a.eval(env).powf(b.eval(env)),
+            Func(ref f, ref a) => f.eval(a.eval(env)),
+            Lit(a) => a,
+            Var(a) => env[a],
+        }
+    }
+}
+
+impl BaseExpr<String> {
+    pub fn eval<S: BuildHasher>(&self, env: &HashMap<&str, f64, S>) -> Result<f64, EvalError> {
+        use self::BaseExpr::*;
         match *self {
             Add(ref a, ref b) => Ok(a.eval(env)? + b.eval(env)?),
             Sub(ref a, ref b) => Ok(a.eval(env)? - b.eval(env)?),
@@ -118,27 +243,27 @@ impl Expr {
             Var(ref a) => env.get(a.as_str()).cloned().ok_or_else(|| EvalError::UnknownVar(a)),
         }
     }
+}
 
-    pub fn vars(&self) -> HashMap<&str, f64> {
-        use self::Expr::*;
-        match *self {
-            Add(ref a, ref b) |
-            Sub(ref a, ref b) |
-            Mul(ref a, ref b) |
-            Div(ref a, ref b) |
-            Pow(ref a, ref b) => {
-                let mut r = a.vars();
-                r.extend(b.vars());
-                r
-            }
-            Func(_, ref a) => a.vars(),
-            Lit(_) => HashMap::new(),
-            Var(ref a) => {
-                let mut r = HashMap::new();
-                r.insert(a.as_str(), 0.0);
-                r
-            }
-        }
+impl CompiledExpr {
+    pub fn vars(&self) -> Vec<&str>{
+        self.varmap.keys().map(|s| s.as_str()).collect::<Vec<_>>()
+    }
+    pub fn set_var(&mut self, name: &str, value: f64) {
+        self.varmap.get(name).cloned().map(|i| self.varstore[i] = value);
+    }
+    pub fn eval(&self) -> f64 {
+        self.expr.eval(&self.varstore)
+    }
+    pub fn bind2(self, v1: &str, v2: &str) -> Box<FnMut(f64, f64) -> f64> {
+        let CompiledExpr { expr, varmap, mut varstore } = self;
+        let v1 = varmap.get(v1).cloned();
+        let v2 = varmap.get(v2).cloned();
+        Box::new(move |x, y| {
+            v1.map(|v1| varstore[v1] = x);
+            v2.map(|v2| varstore[v2] = y);
+            expr.eval(&varstore)
+        })
     }
 }
 
@@ -174,9 +299,9 @@ impl Debug for Equation {
     }
 }
 
-impl Debug for Expr {
+impl<V: Debug> Debug for BaseExpr<V> {
     fn fmt(&self, fmt: &mut Formatter) -> Result<(), fmt::Error> {
-        use self::Expr::*;
+        use self::BaseExpr::*;
         match *self {
             Add(ref a, ref b) => write!(fmt, "({:?} + {:?})", a, b),
             Sub(ref a, ref b) => write!(fmt, "({:?} - {:?})", a, b),
@@ -185,7 +310,7 @@ impl Debug for Expr {
             Pow(ref a, ref b) => write!(fmt, "({:?} ^ {:?})", a, b),
             Func(ref f, ref a) => write!(fmt, "({:?}({:?}))", f, a),
             Lit(a) => write!(fmt, "{:?}", a),
-            Var(ref a) => write!(fmt, "{}", a),
+            Var(ref a) => write!(fmt, "Var({:?})", a),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ extern crate cairo;
 extern crate test;
 extern crate fnv;
 extern crate meval;
+#[macro_use]
+extern crate lazy_static;
 
 // TODO: use meval
 mod expr;
@@ -68,7 +70,7 @@ fn main0() -> Result<(), Box<Error>> {
         Inhibit(false)
     });
 
-    let eq: Rc<RefCell<Option<expr::Equation>>> = Default::default();
+    let eq: Rc<RefCell<Option<Box<FnMut(f64, f64) -> f64>>>> = Default::default();
 
     plot_btn.connect_clicked(
         cloning!(eq, implicit_eqn_entry, info_bar_revealer, info_label, drawing => move |_| {
@@ -78,21 +80,25 @@ fn main0() -> Result<(), Box<Error>> {
                 let new_eq =
                     implicit_eqn_entry.get_text().ok_or("internal: no text")
                     .and_then(|s| expr_parser::parse_Equation(&s).map_err(|_| "parse error"))
-                    .and_then(|ne|
-                              if ne.vars().keys().all(|v| v == &"x" || v == &"y") {
-                                  Ok(ne)
+                    .and_then(|ne| {
+                              let cne = ne.to_diff().compile();
+                              if cne.vars().iter().all(|v| v == &"x" || v == &"y") {
+                                  Ok(cne.bind2("x","y"))
                               } else {
                                   Err("free variables")
                               }
-                    );
+                    });
                 match new_eq {
-                    Ok(_) => info_bar_revealer.set_reveal_child(false),
+                    Ok(ne) => {
+                        info_bar_revealer.set_reveal_child(false);
+                        Some(ne)
+                    }
                     Err(e) => {
                         info_label.set_text(e);
                         info_bar_revealer.set_reveal_child(true);
+                        None
                     }
                 }
-                new_eq.ok()
             }
         }));
 
@@ -100,18 +106,17 @@ fn main0() -> Result<(), Box<Error>> {
         info_bar_revealer.set_reveal_child(false);
     }));
 
+
     drawing.connect_draw(cloning!(eq => move |_, ctx| {
-        let mut map = HashMap::<&str, f64>::with_capacity(2);
+        let mut eq = eq.borrow_mut();
+        let mut const_zero = |_,_| 0.0;
+        let f = if let Some(ref mut eq) = *eq {
+            &mut **eq
+        } else {
+            &mut const_zero
+        };
         marching_squares::marching_squares(ctx,
-                                           |x, y| {
-                                               map.insert("x", x);
-                                               map.insert("y", y);
-                                               if let Some(ref eq) = *eq.borrow() {
-                                                   eq.eval_diff(&map).expect("eval in mar_sq")
-                                               } else {
-                                                   0.0
-                                               }
-                                           },
+                                           f,
                                            &(-10.0..10.0),
                                            256,
                                            &(-10.0..10.0),
@@ -145,21 +150,21 @@ fn parser() {
     assert!(expr_parser::parse_Equation("cos(x) + cos(y) = 1/2").is_ok());
 }
 
-#[test]
-fn expr_vars() {
-    use expr::*;
-    fn ck_vars(e: Expr, i: &[&str]) -> bool {
-        i.into_iter().collect::<HashSet<_>>() == e.vars().keys().collect()
-    }
-    use expr::helper::*;
-    assert!(ck_vars(func(KnownFunc::Sine, lit(1.0)), &[]));
-    assert!(ck_vars(func(KnownFunc::Sine, mul(var("eee"), var("qqq"))),
-                    &["eee", "qqq"]));
-    let abc: Vec<String> = (0x61..0x7A).map(|c| char::from(c).to_string()).collect();
-    let abc: Vec<&str> = abc.iter().map(|s| s.as_str()).collect();
-    assert!(ck_vars(abc.iter().cloned().map(var).fold(lit(0.0), add),
-                    abc.as_slice()));
-}
+// #[test]
+// fn expr_vars() {
+//     use expr::*;
+//     fn ck_vars(e: Expr, i: &[&str]) -> bool {
+//         i.into_iter().collect::<HashSet<_>>() == e.vars().keys().collect()
+//     }
+//     use expr::helper::*;
+//     assert!(ck_vars(func(KnownFunc::Sine, lit(1.0)), &[]));
+//     assert!(ck_vars(func(KnownFunc::Sine, mul(var("eee"), var("qqq"))),
+//                     &["eee", "qqq"]));
+//     let abc: Vec<String> = (0x61..0x7A).map(|c| char::from(c).to_string()).collect();
+//     let abc: Vec<&str> = abc.iter().map(|s| s.as_str()).collect();
+//     assert!(ck_vars(abc.iter().cloned().map(var).fold(lit(0.0), add),
+//                     abc.as_slice()));
+// }
 
 
 const BENCH_EXPR: &'static str = "abs(sin(x + 1) * (x^2 + x + 1))";
@@ -182,18 +187,19 @@ fn my_expr_eval_fnvhash_bench(b: &mut test::Bencher) {
         e.eval(&map);
     })
 }
-#[bench]
-fn my_expr_eval_siphash_bench(b: &mut test::Bencher) {
-    let e = expr_parser::parse_Expr(BENCH_EXPR).unwrap();
-    let mut map = HashMap::<&str, f64>::with_capacity(1);
-    b.iter(move || {
-        // 193ns +/- 4
-        map.insert("x", 1.0);
-        e.eval(&map);
-    })
-}
+// #[bench]
+// fn my_expr_eval_siphash_bench(b: &mut test::Bencher) {
+//     let e = expr_parser::parse_Expr(BENCH_EXPR).unwrap();
+//     let mut map = HashMap::<&str, f64>::with_capacity(1);
+//     b.iter(move || {
+//         // 193ns +/- 4
+//         map.insert("x", 1.0);
+//         e.eval(&map);
+//     })
+// }
 
-const BENCH_EXPR_2: &'static str = "abs(sin(x + 1) * (x^2 + x + 1)) + abs(sin(y + 1) * (y^2 + y + 1))";
+const BENCH_EXPR_2: &'static str = "abs(sin(x + 1) * (x^2 + x + 1)) + abs(sin(y + 1) * (y^2 + y + \
+                                    1))";
 // const BENCH_EXPR_2: &'static str = "x*x*x*x*x*y";
 #[bench]
 fn meval_expr_eval2_bench(b: &mut test::Bencher) {
@@ -214,40 +220,72 @@ fn my_expr_eval2_fnvhash_bench(b: &mut test::Bencher) {
     })
 }
 
+// #[bench]
+// fn my_expr_eval_bench(b: &mut test::Bencher) {
+//     let e = expr_parser::parse_Expr("cos(1.5*x) + cos(y) - 1").unwrap();
+//     let mut map = FnvHashMap::<&str, f64>::with_capacity_and_hasher(2, Default::default());
+//     map.insert("x", 2.0);
+//     map.insert("y", 3.0);
+//     b.iter(move || {
+//         for _ in 0..1000 {
+//             e.eval(&map);
+//         }
+//     })
+// }
+
 #[bench]
-fn my_expr_eval_bench(b: &mut test::Bencher) {
-    let e = expr_parser::parse_Expr("cos(1.5*x) + cos(y) - 1").unwrap();
-    let mut map = FnvHashMap::<&str, f64>::with_capacity_and_hasher(2, Default::default());
-    map.insert("x", 2.0);
-    map.insert("y", 3.0);
+fn my_comp_expr_eval_bench(b: &mut test::Bencher) {
+    let mut e = expr_parser::parse_Expr("cos(1.5*x) + cos(y) - 1")
+        .unwrap()
+        .compile()
+        .bind2("x", "y");
     b.iter(move || {
         for _ in 0..1000 {
-            e.eval(&map);
+            // 80.9ns +/- 1.0
+            e(2.0, 3.0);
         }
     })
 }
 
 #[bench]
-fn fnvhashget(b: &mut test::Bencher) {
-    let mut map = FnvHashMap::<&str, f64>::with_capacity_and_hasher(1, Default::default());
-    map.insert("0", 1.0);
-    b.iter(|| for _ in 0..1000 { map.get("0"); });
+fn teast(b: &mut test::Bencher) {
+    b.iter(|| for _ in 0..1000 {
+        test::black_box(())
+    });
 }
-#[bench]
-fn fnvhashinsert(b: &mut test::Bencher) {
-    let mut map = FnvHashMap::<&str, f64>::with_capacity_and_hasher(1, Default::default());
-    b.iter(|| for _ in 0..1000 { map.insert("0", 1.0); });
-}
-#[bench]
-fn vec_write(b: &mut test::Bencher) {
-    let mut v = vec![0];
-    b.iter(|| for _ in 0..1000 { unsafe { *v.get_unchecked_mut(0) = test::black_box(1); } });
-}
-#[bench]
-fn vec_read(b: &mut test::Bencher) {
-    let mut v = vec![0];
-    b.iter(|| for _ in 0..1000 { unsafe { test::black_box(v.get_unchecked(0)); } });
-}
+
+// #[bench]
+// fn expr_setvar_noop(b: &mut test::Bencher) {
+//     let mut e = expr_parser::parse_Expr("a*b*c*d*e*f*g*x").unwrap().compile();
+//     b.iter(|| for _ in 0..1000 { e.set_var("z", 1.0) });
+// }
+// #[bench]
+// fn expr_setvar(b: &mut test::Bencher) {
+//     let mut e = expr_parser::parse_Expr("a*b*c*d*e*f*g*x").unwrap().compile();
+//     b.iter(|| /* 4.1ns +/- 0.1 */ for _ in 0..1000 { e.set_var("x", 1.0) });
+// }
+
+// #[bench]
+// fn fnvhashget(b: &mut test::Bencher) {
+//     let mut map = FnvHashMap::<&str, f64>::with_capacity_and_hasher(1, Default::default());
+//     map.insert("0", 1.0);
+//     b.iter(|| for _ in 0..1000 { map.get("0"); });
+// }
+// #[bench]
+// fn fnvhashinsert(b: &mut test::Bencher) {
+//     let mut map = FnvHashMap::<&str, f64>::with_capacity_and_hasher(1, Default::default());
+//     b.iter(|| for _ in 0..1000 { map.insert("0", 1.0); });
+// }
+// #[bench]
+// fn vec_write(b: &mut test::Bencher) {
+//     let mut v = vec![0];
+//     b.iter(|| for _ in 0..1000 { unsafe { *v.get_unchecked_mut(0) = test::black_box(1); } });
+// }
+// #[bench]
+// fn vec_read(b: &mut test::Bencher) {
+//     let mut v = vec![0];
+//     b.iter(|| for _ in 0..1000 { unsafe { test::black_box(v.get_unchecked(0)); } });
+// }
 
 #[bench]
 fn plot_bench(b: &mut test::Bencher) {
@@ -255,16 +293,12 @@ fn plot_bench(b: &mut test::Bencher) {
     let ctx = cairo::Context::new(&surf);
 
     let eq = expr_parser::parse_Equation("x^2 + y^2 = 5^2").unwrap();
+    let mut f = eq.to_diff().compile().bind2("x", "z");
 
     b.iter(move || {
-        // 6.383ms +/- 0.322
-        let mut map = FnvHashMap::<&str, f64>::with_capacity_and_hasher(2, Default::default());
+        // 4.138ms +/- 0.223
         marching_squares::marching_squares(&ctx,
-                                           |x, y| {
-                                               map.insert("x", x);
-                                               map.insert("y", y);
-                                               eq.eval_diff(&map).expect("eval in mar_sq")
-                                           },
+                                           |x, y| f(x, y),
                                            &(-10.0..10.0),
                                            256,
                                            &(-10.0..10.0),
